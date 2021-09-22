047987cf2849162fd1c32eed8d49c3a2997dc3ee
Tiago Welter <tiago.welter@engdb.com.br>
2021-09-22 19:43:55 -0300

    Aula 7

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -10,7 +10,11 @@ export default class ProductCard extends LightningElement {
     }
     set product(value){
 
-        this._product = {id: value.Id, nome : value.Name};
+        let image = value.DisplayUrl == null ? 'https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg' : value.DisplayUrl;
+
+        let priceProduct = value.Price__c == null ? 0 : value.Price__c;
+
+        this._product = {id: value.Id, nome : value.Name, imagem: image, preco: priceProduct};
 
     }
 
