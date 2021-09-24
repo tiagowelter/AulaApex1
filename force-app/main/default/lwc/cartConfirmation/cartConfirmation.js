2adf7cac51bfa9af9b41d05ea3268578cb7d43e4
Tiago Welter <tiago.welter@engdb.com.br>
2021-09-23 21:58:40 -0300

    Aula 8

--- force-app/main/default/lwc/cartConfirmation/cartConfirmation.js
+++ force-app/main/default/lwc/cartConfirmation/cartConfirmation.js
@@ -1,7 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import { NavigationMixin} from 'lightning/navigation';
 
 export default class CartConfirmation extends NavigationMixin(LightningElement) {
+
+    @api products;
+    @api nameopp;
+    @api dateopp;
 
     closeModal(){
         const cancelEvent = new CustomEvent( "cancelconfirmation", {
